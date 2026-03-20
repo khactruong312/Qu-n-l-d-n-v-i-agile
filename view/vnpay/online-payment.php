2033e1a38636dc0fea12fbc27d4fa6064100e62f
datbinn <datbinn06@gmail.com>
2026-03-20 08:22:38 +0700

    upup

--- view/vnpay/online-payment.php
+++ view/vnpay/online-payment.php
@@ -74,7 +74,7 @@ if (isset($_POST['redirect'])) {
     $vnp_Url = $vnp_Url . "?" . $query;
     if (isset($vnp_HashSecret)) {
         $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
-        $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+        $vnp_Url .= '&vnp_SecureHash=' . $vnpSecureHash;
     }
     $returnData = array(
         'code' => '00',
